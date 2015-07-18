36faf42469adba0cea2c206ff62b802ea27fbc28
vassildinev <vassildinev@gmail.com>
2015-07-18 12:08:41 +0300

    Final version

--- 01.HTML5Canvas/Snake/snake.js
+++ 01.HTML5Canvas/Snake/snake.js
@@ -1,34 +1,51 @@
 window.onload = function () {
-    Array.prototype.enqueue = function (item) {
-        this.push(item);
-        return this;
-    };
-
-    Array.prototype.peekFirst = function () {
-        return this[0];
-    };
-
-    var canvas, ctx, isDead, score = 0, logger = [], i, len, snake, coin, fps = 100, currentFrame = 0, count = 1,
+    var gameCanvas,
+        gameCtx,
+        scoreboardCanvas,
+        scoreboardCtx,
+        isDead,
+        score = 0,
+        logger = [],
+        i,
+        j = localStorage.length,
+        len,
+        localStorageCopy = [],
+        snake,
+        coin,
+        fps = 100,
+        currentFrame = 0,
+        tailObjectsCount = 1,
         CONSTANTS = {
             SNAKE_RADIUS: 7,
-            COIN_RADIUS: 7
+            COIN_RADIUS: 7,
+            FRAME_SETBACK: 13
         };
 
-    function loadCanvas() {
-        canvas = document.createElement('canvas');
-        canvas.height = 400;
-        canvas.width = 600;
-        canvas.style.border = '1px solid black';
-        document.body.appendChild(canvas);
+    function loadGameCanvas() {
+        gameCanvas = document.createElement('canvas');
+        gameCanvas.height = 300;
+        gameCanvas.width = 400;
+        gameCanvas.style.border = '1px solid black';
+        document.body.appendChild(gameCanvas);
 
-        ctx = canvas.getContext('2d');
+        gameCtx = gameCanvas.getContext('2d');
     }
 
-    function getSnakeObject(canvas) {
+    function loadScoreboardCanvas() {
+        scoreboardCanvas = document.createElement('canvas');
+        scoreboardCanvas.height = 300;
+        scoreboardCanvas.width = 150;
+        scoreboardCanvas.style.border = '1px solid black';
+        document.body.appendChild(scoreboardCanvas);
+
+        scoreboardCtx = scoreboardCanvas.getContext('2d');
+    }
+
+    function getSnakeObject() {
         return {
             head: {
-                x: 0.5 * canvas.width,
-                y: 0.5 * canvas.height,
+                x: 0.5 * gameCanvas.width,
+                y: 0.5 * gameCanvas.height,
                 r: CONSTANTS.SNAKE_RADIUS,
                 speed: {
                     x: 1,
@@ -40,52 +57,163 @@ window.onload = function () {
         };
     }
 
-    function getCoinObject(canvas) {
+    function getCoinObject() {
         return {
-            x: CONSTANTS.COIN_RADIUS + Math.random() * (canvas.width - 2 * CONSTANTS.COIN_RADIUS),
-            y: CONSTANTS.COIN_RADIUS + Math.random() * (canvas.height - 2 * CONSTANTS.COIN_RADIUS)
+            x: CONSTANTS.COIN_RADIUS + Math.random() * (gameCanvas.width - 2 * CONSTANTS.COIN_RADIUS),
+            y: CONSTANTS.COIN_RADIUS + Math.random() * (gameCanvas.height - 2 * CONSTANTS.COIN_RADIUS)
         }
     }
 
-    function loadGameObjects(canvas) {
-        snake = getSnakeObject(canvas);
-        coin = getCoinObject(canvas);
+    function loadGameObjects() {
+        snake = getSnakeObject();
+        coin = getCoinObject();
     }
 
-    function drawSnake(ctx, snake) {
+    function drawSnake() {
         // head
-        ctx.beginPath();
-        ctx.arc(snake.head.x, snake.head.y, CONSTANTS.SNAKE_RADIUS, 0, 2 * Math.PI);
-        ctx.fillStyle = 'rgb(255, 0, 0)';
-        ctx.fill();
-        ctx.closePath();
+        gameCtx.beginPath();
+        gameCtx.arc(snake.head.x, snake.head.y, CONSTANTS.SNAKE_RADIUS, 0, 2 * Math.PI);
+        gameCtx.fillStyle = 'rgb(255, 0, 0)';
+        gameCtx.fill();
+        gameCtx.closePath();
 
         // tail
-        for(i = 0, len = snake.tail.length; i < len; i += 1) {
+        for (i = 0, len = snake.tail.length; i < len; i += 1) {
             var currentTailObject = snake.tail[i];
-            ctx.beginPath();
-            ctx.arc(currentTailObject.x, currentTailObject.y, CONSTANTS.SNAKE_RADIUS, 0, 2 * Math.PI);
-            ctx.fillStyle = 'rgb(0, 255, 0)';
-            ctx.fill();
-            ctx.closePath();
+            gameCtx.beginPath();
+            gameCtx.arc(currentTailObject.x, currentTailObject.y, CONSTANTS.SNAKE_RADIUS, 0, 2 * Math.PI);
+            gameCtx.fillStyle = 'rgb(0, 255, 0)';
+            gameCtx.fill();
+            gameCtx.closePath();
         }
     }
 
-    function drawCoin(ctx, coin) {
-        ctx.beginPath();
-        ctx.arc(coin.x, coin.y, CONSTANTS.COIN_RADIUS, 0, 2 * Math.PI);
-        ctx.fillStyle = 'rgb(220, 230, 0)';
-        ctx.fill();
-        ctx.closePath();
+    function drawCoin() {
+        gameCtx.beginPath();
+        gameCtx.arc(coin.x, coin.y, CONSTANTS.COIN_RADIUS, 0, 2 * Math.PI);
+        gameCtx.fillStyle = 'rgb(100, 200, 255)';
+        gameCtx.fill();
+        gameCtx.closePath();
     }
 
-    function render(ctx) {
-        clearCanvas(canvas, ctx);
-        drawCoin(ctx, coin);
-        drawSnake(ctx, snake);
+    function drawScore() {
+        scoreboardCtx.beginPath();
+        scoreboardCtx.fillStyle = 'black';
+        scoreboardCtx.font = '16px Arial';
+        scoreboardCtx.fontWeight = 100;
+        scoreboardCtx.fillText('Score: ' + score, 2, 16);
+        scoreboardCtx.fillText('Speed: ' + (10 - (1000 / fps - 1) | 0), 2, 32);
+        scoreboardCtx.closePath();
+    }
+
+    function drawHighScore() {
+        scoreboardCtx.beginPath();
+        scoreboardCtx.fillStyle = 'black';
+        scoreboardCtx.font = '16px Arial';
+        scoreboardCtx.fontWeight = 100;
+        scoreboardCtx.fillText('---HIGH SCORES---', 2, 64);
+
+        localStorageCopy = [].slice.apply(localStorage)
+            .filter(function (item) {
+                return item !== undefined;
+            });
+
+        for (i = 0, len = localStorageCopy.length; i < len; i += 1) {
+            localStorageCopy[i] = localStorageCopy[i].split('-');
+        }
+
+        localStorageCopy = localStorageCopy.sort(function (x, y) {
+            return y[1] - x[1] ;
+        });
+
+
+
+        for (i = 0, len = localStorageCopy.length; i < len && i < 5; i += 1) {
+            scoreboardCtx.fillText(i + 1 + '. ' + localStorageCopy[i][0] + ' - ' + localStorageCopy[i][1], 2, (i + 1) * 16 + 64);
+        }
+
+        scoreboardCtx.closePath();
+    }
+
+    function drawInformation() {
+        scoreboardCtx.beginPath();
+        scoreboardCtx.fillStyle = 'black';
+        scoreboardCtx.font = '16px Tahoma';
+        scoreboardCtx.fillText('Press Space for', 2, 240);
+        scoreboardCtx.fillText('new game', 2, 256);
+        scoreboardCtx.fillText('or F5 for new game', 2, 272);
+        scoreboardCtx.fillText('and scoreboard', 2, 288);
+    }
+
+    function clearGameCtx() {
+        gameCtx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    }
+
+    function clearScoreboardCtx() {
+        scoreboardCtx.clearRect(0, 0, scoreboardCanvas.width, scoreboardCanvas.height);
+    }
+
+    function render() {
+        clearGameCtx();
+        clearScoreboardCtx();
+        drawCoin();
+        drawSnake();
+        drawScore();
+        drawHighScore();
+        drawInformation();
+    }
+
+    function checkSnakePosition() {
+        if (snake.head.x + CONSTANTS.SNAKE_RADIUS >= gameCanvas.width ||
+            snake.head.x <= CONSTANTS.SNAKE_RADIUS ||
+            snake.head.y + CONSTANTS.SNAKE_RADIUS >= gameCanvas.height ||
+            snake.head.y <= CONSTANTS.SNAKE_RADIUS) {
+            isDead = true;
+        }
+
+        for (i = 0, len = snake.tail.length; i < len; i += 1) {
+            var currentTailObject = snake.tail[i];
+            if ((snake.head.x - currentTailObject.x) * (snake.head.x - currentTailObject.x) +
+                (snake.head.y - currentTailObject.y) * (snake.head.y - currentTailObject.y) <
+                CONSTANTS.SNAKE_RADIUS * CONSTANTS.SNAKE_RADIUS) {
+                isDead = true;
+            }
+        }
+
+        if (isDead) {
+            var person = prompt('Please enter your name:');
+            while (person === '' || prompt === null) {
+                person = prompt('Please enter your name:');
+            }
+
+            localStorage.setItem(j, person + ' - ' + score);
+            j = localStorage.length;
+        }
+
+        if ((snake.head.x - coin.x) * (snake.head.x - coin.x) +
+            (snake.head.y - coin.y) * (snake.head.y - coin.y) <
+            4 * CONSTANTS.SNAKE_RADIUS * CONSTANTS.SNAKE_RADIUS) {
+            score += 1;
+            fps += score * 2;
+            snake.tail.push({
+                x: undefined,
+                y: undefined,
+                frame: currentFrame - CONSTANTS.FRAME_SETBACK * tailObjectsCount
+            });
+
+            tailObjectsCount++;
+
+            coin = getCoinObject();
+        }
+
+    }
+
+    function logHeadPosition(position) {
+        logger.push(position);
     }
 
     function update() {
+        // log last snake head position
         logHeadPosition({
             x: snake.head.x,
             y: snake.head.y
@@ -96,86 +224,80 @@ window.onload = function () {
         snake.head.y += snake.head.speed.y;
 
         // tail
-        for(i = 0, len = snake.tail.length; i < len; i += 1) {
+        for (i = 0, len = snake.tail.length; i < len; i += 1) {
             snake.tail[i].x = logger[snake.tail[i].frame].x;
             snake.tail[i].y = logger[snake.tail[i].frame].y;
 
             snake.tail[i].frame += 1;
         }
 
-        console.log(snake.tail);
-
-        checkSnakePosition(snake);
+        checkSnakePosition();
     }
 
-    function checkSnakePosition(snake) {
-        if (snake.head.x + CONSTANTS.SNAKE_RADIUS >= canvas.width ||
-            snake.head.x <= CONSTANTS.SNAKE_RADIUS ||
-            snake.head.y + CONSTANTS.SNAKE_RADIUS >= canvas.height ||
-            snake.head.y <= CONSTANTS.SNAKE_RADIUS) {
-            isDead = true;
-        }
-
-        if ((snake.head.x - coin.x) * (snake.head.x - coin.x) + (snake.head.y - coin.y) * (snake.head.y - coin.y) < 4 * CONSTANTS.SNAKE_RADIUS * CONSTANTS.SNAKE_RADIUS) {
-            score += 10;
-            snake.tail.push({
-                frame: currentFrame - 13 * count
-            });
-
-            count++;
-
-            coin = getCoinObject(canvas);
-        }
-
-    }
-
-    function runFrame() {
+    function run() {
         update();
-        render(ctx);
+        render();
+
+        if (!isDead) {
+            setTimeout(run, 1000 / fps);
+        }
+
         currentFrame += 1;
-        if (!isDead) setTimeout(runFrame, 1000 / fps);
     }
 
-    function clearCanvas(canvas, ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function reset() {
+        isDead = false;
+        fps = 100;
+        score = 0;
+        currentFrame = 0;
+        logger = [];
+        tailObjectsCount = 1;
+        loadGameObjects();
     }
 
-    function addEventListeners() {
+    function addGameEventListener() {
         addEventListener('keydown', function (e) {
-            changeSnakeDirection(e.keyCode);
+            var keyCode = e.keyCode;
+
+            if (keyCode === 116) {
+                localStorage.clear();
+            }
+
+            if (keyCode === 32) {
+                reset();
+                run();
+            }
+            if (keyCode === 37) {
+                snake.head.speed.x = -1;
+                snake.head.speed.y = 0;
+            }
+
+            if (keyCode === 38) {
+                snake.head.speed.x = 0;
+                snake.head.speed.y = -1;
+            }
+
+            if (keyCode === 39) {
+                snake.head.speed.x = 1;
+                snake.head.speed.y = 0;
+            }
+
+            if (keyCode === 40) {
+                snake.head.speed.x = 0;
+                snake.head.speed.y = 1;
+            }
         });
     }
 
-    function logHeadPosition(position) {
-            logger.push(position);
+    function initialize() {
+        loadGameCanvas();
+        loadScoreboardCanvas();
+        loadGameObjects();
+        addGameEventListener();
     }
 
-    function changeSnakeDirection(keyCode) {
-        if (keyCode === 37) {
-            snake.head.speed.x = -1;
-            snake.head.speed.y = 0;
-        }
-
-        if (keyCode === 38) {
-            snake.head.speed.x = 0;
-            snake.head.speed.y = -1;
-        }
-
-        if (keyCode === 39) {
-            snake.head.speed.x = 1;
-            snake.head.speed.y = 0;
-        }
-
-        if (keyCode === 40) {
-            snake.head.speed.x = 0;
-            snake.head.speed.y = 1;
-        }
-    }
-
-    (function startGame() {
-        loadCanvas(canvas, ctx);
-        addEventListeners();
-        loadGameObjects(canvas);
-        runFrame();
+    (function start() {
+        initialize();
+        run();
     }());
 };
